da8aa10afe775684eb35066332de100d0842a73d
Odell Purifoy <odell.purifoy@freshly.com>
2021-11-08 19:49:36 -0500

    Added Rspec, FactoryBot and Shoulda Matchers gem

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+# 
+# RSpec.describe User, type: :model do
+#   describe "Creating a valid user" do
+#     user = User.create(
+#       last_name: "TestLastName",
+#       email: "test@email.com",
+#       usernmame: 'username'
+#     )
+#
+#     user.save!
+#
+#     it "doest not save without first_name" do
+#       expect
+#     end
+#   end
+# end
